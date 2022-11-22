a6f3c652808f40dcefdf81ee7cc8fe814bd7388f
RodolfoJGonz <rodolfojgs@hotmail.com>
2022-11-22 10:17:27 -0600

    finishing queries

---
+++ games-data/sandbox-crud-operation-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, datetime_value) VALUES (78, 1, '2001-11-22 00:00:00')
+
+--UPDATE sandbox SET int64_value = 41968 WHERE id = 85;
+
+--DELETE FROM sandbox WHERE id = 61;
+
+SELECT * FROM sandbox;
